297c2b50e02e084a751b5eb3c33ab071b8a00922
RicardoPerezJ <ricardo.perezjimenez@gmail.com>
2024-09-02 15:40:54 -0600

    primer commit proyecto moveObject

---
+++ src/main/java/ucr/ac/cr/tm2100g3/moveobjects/MoveObjects.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ucr.ac.cr.tm2100g3.moveobjects;
+
+/**
+ *
+ * @author Ricardo
+ */
+public class MoveObjects {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
